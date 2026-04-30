f3eb96ad7565a055343ad40d1d0f9052a7516a64
breno-concrete <brenocount@gmail.com>
2026-04-30 11:40:51 -0300

    feat(product): JSR-380 validation

--- src/main/java/com/breno/marketplace_test/dtos/LoginRequestDTO.java
+++ src/main/java/com/breno/marketplace_test/dtos/LoginRequestDTO.java
@@ -2,15 +2,15 @@ package com.breno.marketplace_test.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-
+import jakarta.validation.constraints.Size;
 
 //data transfer object para transportas os dados de login do usuário, com validação para garantir que o email e a password sejam fornecidos e que o email seja válido
-
 public record LoginRequestDTO(
         @NotBlank(message = "Email is required") //não vazio
         @Email(message = "Email must be valid") //formato correto e domínio correto
         String email,
 
-        @NotBlank(message = "Password is required") //não vazio
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
         String password
 ) {}
