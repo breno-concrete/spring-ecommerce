b4f9becc1daa9e83384739d9b869407044a05a29
breno-concrete <brenocount@gmail.com>
2026-05-02 23:49:24 -0300

    fix(auth): fix user repository return type and security filter cast

--- src/main/java/com/breno/marketplace_test/repositories/UserRepository.java
+++ src/main/java/com/breno/marketplace_test/repositories/UserRepository.java
@@ -4,8 +4,10 @@ import com.breno.marketplace_test.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
