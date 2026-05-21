2f23a32e76d5cccdac2266327132c8393b7a29fb
breno-concrete <brenocount@gmail.com>
2026-05-21 13:01:37 -0300

    feat(product): add ProductFilterDTO and update ProductRepository for filtering support

--- src/main/java/com/breno/marketplace_test/repositories/ProductRepository.java
+++ src/main/java/com/breno/marketplace_test/repositories/ProductRepository.java
@@ -2,8 +2,11 @@ package com.breno.marketplace_test.repositories;
 
 import com.breno.marketplace_test.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-}
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    }
+
 
